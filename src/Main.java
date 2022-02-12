a7bfbb236877fc8b174a02f77ebb3da2aa3667d8
PierreFeo <feokristov.petr@gmail.com>
2022-02-12 13:34:40 +1000

    2+3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello world, people!!");
+        System.out.println("Hello world, people!!");
+        int a = 2;
+        int b = 3;
+        int c = a + b;
+        System.out.println("2 + 3= " + c);
     }
 }
