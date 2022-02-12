7b84e42d79a412841165efa803c8b32b7172c5ec
PierreFeo <feokristov.petr@gmail.com>
2022-02-12 14:26:17 +1000

    good

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,15 @@ public class Main {
         System.out.println("Hello world, people!!");
         int a = 2;
         int b = 3;
-        int c = a + b;
-        System.out.println("2 + 3= " + c);
+        int c = 0;
 
         while (true) {
-            if (c < 5) {
-                System.out.println("Цифра меньше 5, идем дальше!");
-            } else System.out.println("опа!Пятерка!!");
-            break;
+            c++;
+            System.out.println("Цифра меньше 5, идем дальше!");
+            if (c >= 5) {
+                break;
+            }
         }
+        System.out.println("Пятерка!!");
     }
 }
